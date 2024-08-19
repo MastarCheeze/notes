16a6464bdc8fd14ce9720b7accb0277b006d04bb
MastarCheeze <mon10180218@gmail.com>
2024-08-19 13:52:55 +0800

    refactor code

--- scripts/src/compile-dir.ts
+++ scripts/src/compile-dir.ts
@@ -1,151 +1,118 @@
 import fs from "node:fs/promises";
 import path from "node:path";
+import assert from "node:assert";
 
 import { parse } from "./parse.js";
 import { compile } from "./compile.js";
-import type { Metadata } from "./parse.js";
-import type { CompileConfig, DirTree } from "./compile.js";
+import type { CompileConfig, Breadcrumb, DirTree } from "./compile.js";
 
 async function main() {
-    const breadcrumb: CompileConfig["breadcrumb"] = []; // current location in the filesystem
+    const breadcrumb: Breadcrumb = []; // current location in the filesystem
 
-    async function recursiveCompile(dirSrc: string) {
+    async function recursiveCompile(currDirSrc: string) {
         const dirTree: DirTree = []; // subtree of child files and folders
 
         // create directory
-        const dirOut = dirSrc.replace(SRC_DIR, OUT_DIR);
-        const dirRel = path.relative(OUT_DIR, dirOut);
-        let dirName = path.basename(dirOut);
-        await fs.mkdir(dirOut);
+        const currDirOut = currDirSrc.replace(SRC_DIR, OUT_DIR);
+        const currDirLink = path.relative(OUT_DIR, currDirOut);
+        await fs.mkdir(currDirOut);
 
         // check if index.md exists
-        let hasIndexFile = true;
-        let compileIndexFileFunc;
+        const indexSrc = path.join(currDirSrc, "index.md");
+        let hasIndexMd = true;
+        let indexParsed = null;
+        let indexMetadata = null;
         try {
-            await fs.stat(path.join(dirSrc, "index.md"));
+            await fs.stat(indexSrc);
         } catch {
-            hasIndexFile = false;
+            hasIndexMd = false;
         }
-        if (hasIndexFile) {
-            const src = path.join(dirSrc, "index.md");
-            const out = path.join(dirOut, "index.html");
-
-            const markdown = await fs.readFile(src, { encoding: "utf-8" });
-            const { parsed, metadata } = parse(markdown);
-
-            if (parsed.length !== 0) {
-                compileIndexFileFunc = compileIndexFile.bind(
-                    null,
-                    parsed,
-                    metadata,
-                    out,
-                    {
-                        dirName,
-                        dirRel,
-                        breadcrumb,
-                        dirTree,
-                    }
-                );
-
-                breadcrumb.push([metadata.title ?? dirName, dirRel]);
-                dirName = metadata.title ?? dirName;
-            } else {
-                breadcrumb.push([metadata.title ?? dirName, null]);
-                dirName = metadata.title ?? dirName;
-                hasIndexFile = false
-            }
-
-        } else {
-            breadcrumb.push([dirName, null]);
+        if (hasIndexMd) {  // parse index.md to prepare for compiling
+            const markdown = await fs.readFile(indexSrc, { encoding: "utf-8" });
+            ({ parsed: indexParsed, metadata: indexMetadata } = parse(markdown));
         }
+        const currDirTitle = indexMetadata?.title ?? path.basename(currDirOut);
 
-        // loop through all files in directory
-        const entries = await fs.readdir(dirSrc, { withFileTypes: true });
+        // link to index.html only if render dir tree is enabled
+        if (indexMetadata?.dirTree)
+            breadcrumb.push([currDirTitle, currDirLink]);
+        else
+            breadcrumb.push([currDirTitle, null]);
+
+        // loop through all entries in directory
+        const entries = await fs.readdir(currDirSrc, { withFileTypes: true });
         for (const entry of entries) {
             const src = path.join(entry.parentPath, entry.name);
 
-            if (entry.isDirectory()) {
+            if (entry.isDirectory()) {  // deeper recursion if directory
                 const subDirTree = await recursiveCompile(src);
                 if (subDirTree !== undefined)
                     dirTree.push(subDirTree);
-            } else if (entry.name === "index.md") {
-                ; // skip index.md to compile later
-            } else if (entry.name.endsWith(".md")) {
-                const out = src.replace(SRC_DIR, OUT_DIR).replace(".md", ".html");
-                const rel = path.relative(OUT_DIR, out);
 
+            } else if (entry.name === "index.md") {  // skip if index.md because already parsed
+                ;
+
+            } else if (entry.name.endsWith(".md")) {  // parse and compile all other md files
+                const out = src.replace(SRC_DIR, OUT_DIR).replace(".md", ".html");
+
+                // parse
                 const markdown = await fs.readFile(src, { encoding: "utf-8" });
                 const { parsed, metadata } = parse(markdown);
 
-                const title = await compileMarkdownFile(parsed, metadata, out, { breadcrumb });
+                // compile
+                const title = metadata.title ?? path.basename(out);
+                const options: CompileConfig = {
+                    title: title,
+                    breadcrumb: [...breadcrumb, ["📄 " + title, null]],
+                    baseUrl: BASE_URL,
+                };
+                const doc = compile(parsed, "page", options);
 
-                dirTree.push([title, rel]);
-            } else {
+                // write
+                await fs.writeFile(out, doc);
+
+                const link = path.relative(OUT_DIR, out);
+                dirTree.push([title, link]);
+
+            } else {  // copy all other file types as is
                 const out = src.replace(SRC_DIR, OUT_DIR);
                 await fs.copyFile(src, out);
             }
         }
 
+        // compile index.md if only if render dir tree is enabled
+        if (indexMetadata?.dirTree) {
+            assert(indexParsed !== null && indexMetadata !== null);
+            const indexOut = path.join(currDirOut, "index.html");
+
+            // compile
+            const options: CompileConfig = {
+                title: currDirTitle,
+                breadcrumb: breadcrumb,
+                baseUrl: BASE_URL,
+                dirTree: dirTree,
+            };
+            const doc = compile(indexParsed, "index", options);
+
+            // write
+            await fs.writeFile(indexOut, doc);
+
+            dirTree.push([currDirTitle, currDirLink]);
+        }
         breadcrumb.pop();
 
+        // join subtree with parent tree, only if folder has one or more compiled files
         if (dirTree.length > 0) {
-            if (hasIndexFile) {
-                await compileIndexFileFunc!();
-            }
-            return [dirName, (hasIndexFile) ? dirRel : null, dirTree] as [string, string, DirTree]; // join subtree with parent tree
+            return [
+                currDirTitle,
+                // link to index.html only if render dir tree is enabled
+                indexMetadata?.dirTree ? currDirLink : null,
+                dirTree,
+            ] as [string, string, DirTree];
         }
     }
 
     await recursiveCompile(SRC_DIR);
-}
-
-async function compileIndexFile(
-    parsed: string,
-    metadata: Metadata,
-    out: string,
-    config: {
-        dirName: string,
-        dirRel: string,
-        breadcrumb: CompileConfig["breadcrumb"],
-        dirTree: DirTree,
-    }
-) {
-    const options: Record<string, any> = {};
-    options.title = metadata.title ?? config.dirName;
-    options.breadcrumb = [...config.breadcrumb, [options.title, config.dirRel]];
-    options.baseUrl = BASE_URL;
-
-    let doc;
-    if (metadata.dirTree) {
-        options.dirTree = config.dirTree;
-        doc = compile(parsed, "index", options as CompileConfig);
-    } else {
-        doc = compile(parsed, "page", options as CompileConfig);  // compile as normal page if directory tree not needed
-    }
-
-    // write
-    await fs.writeFile(out, doc);
-}
-
-async function compileMarkdownFile(
-    parsed: string,
-    metadata: Metadata,
-    out: string,
-    config: {
-        breadcrumb: CompileConfig["breadcrumb"],
-    },
-) {
-    // compile
-    const options: Record<string, any> = {};
-    options.title = metadata.title ?? path.basename(out);
-    options.breadcrumb = [...config.breadcrumb, ["📄 " + options.title, null]];
-    options.baseUrl = BASE_URL;
-    const doc = compile(parsed, "page", options as CompileConfig);
-
-    // write
-    await fs.writeFile(out, doc);
-
-    return options.title;
 }
 
 function parseArgvAfterFlag(flag: string) {
@@ -168,6 +135,3 @@ const OUT_DIR = parseArgvAfterFlag("-o")[0]!;
 const BASE_URL = parseArgvAfterFlag("--base-url")[0]!;
 
 await main();
-
-// TODO sort files by alphabetical order, folders always on top
-// TODO order metadata for ordering files/folders within toc pages
