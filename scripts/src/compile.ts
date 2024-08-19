37704cc3995e0c4602c3f3bd13f61589fced406f
MastarCheeze <mon10180218@gmail.com>
2024-08-19 14:28:29 +0800

    modularise templates and change replace str

--- scripts/src/compile.ts
+++ scripts/src/compile.ts
@@ -10,30 +10,36 @@ export type CompileConfig = {
     dirTree?: DirTree;
 }
 
-const templatePaths: { [key in PageType]: string } = {
+const BASE_TEMPLATE_PATH = "scripts/src/templates/base.html";
+const TEMPLATE_PATHS: { [key in PageType]: string } = {
     "page": "scripts/src/templates/page.html",
     "index": "scripts/src/templates/index.html",
 }
 
-const templates = Object.fromEntries(Object.keys(templatePaths).map((key) => [key, ""])) as typeof templatePaths;
-for (const [key, path] of Object.entries(templatePaths)) {
-    templates[key as PageType] = await fs.readFile(path, { encoding: "utf-8" })
+const baseTemplate = (await fs.readFile(BASE_TEMPLATE_PATH, { encoding: "utf-8" })).normalize();
+const templates = Object.fromEntries(Object.keys(TEMPLATE_PATHS).map((key) => [key, ""])) as typeof TEMPLATE_PATHS;
+for (const [key, path] of Object.entries(TEMPLATE_PATHS)) {
+    templates[key as PageType] = (await fs.readFile(path, { encoding: "utf-8" })).normalize();
+    console.log(templates[key as PageType]);
 }
+console.log(baseTemplate);
 
 export function compile(contents: string, template: PageType, config: CompileConfig) {
     /**
      * Compile html elements parsed from markdown into a html page
      */
 
-    let doc = templates[template];
+    let doc = baseTemplate;
+    doc = doc.replace("{{template}}", templates[template]);
+    console.log(doc)
 
-    doc = doc.replace("$$content$$", contents);
-    doc = doc.replace("$$title$$", config.title);
-    doc = doc.replace("$$baseUrl$$", config.baseUrl);
-    doc = doc.replace("$$breadcrumb$$", createBreadcrumb(config.breadcrumb));
+    doc = doc.replace("{{content}}", contents);
+    doc = doc.replace("{{title}}", config.title);
+    doc = doc.replace("{{baseUrl}}", config.baseUrl);
+    doc = doc.replace("{{breadcrumb}}", createBreadcrumb(config.breadcrumb));
 
     if (template === "index") {
-        doc = doc.replace("$$dirTree$$", createDirTree(config.dirTree));
+        doc = doc.replace("{{dirTree}}", createDirTree(config.dirTree));
     }
 
     return doc;
