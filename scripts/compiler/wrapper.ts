2da3a651cdc5ebb8677ecc81938f71d5c5a0ec4f
MastarCheeze <mon10180218@gmail.com>
2024-09-13 08:41:06 +0800

    unit support katex

--- scripts/compiler/wrapper.ts
+++ scripts/compiler/wrapper.ts
@@ -1,7 +1,11 @@
 import { marked } from "marked";
 import katex from "marked-katex-extension";
 
-marked.use({ breaks: true }).use(katex());
+marked.use({ breaks: true }).use(
+    katex({
+        macros: { "\\unit": "\\text{~#1}" },
+    })
+);
 
 export function compile(markdown: string) {
     return marked.parse(markdown) as string;
