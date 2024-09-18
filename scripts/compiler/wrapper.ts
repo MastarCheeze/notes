88a469062158be999ccb6b434cd4c4432d5264fd
MastarCheeze <mon10180218@gmail.com>
2024-09-19 07:34:31 +0800

    delete old scripts

--- scripts/compiler/wrapper.ts
+++
@@ -1,8 +0,0 @@
-import { marked } from "marked";
-import katex from "marked-katex-extension";
-
-marked.use({ breaks: true }).use(katex());
-
-export function compile(markdown: string) {
-    return marked.parse(markdown) as string;
-}
