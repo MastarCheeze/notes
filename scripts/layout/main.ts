fb3310cd89fb6774f3d0623993ecdee2d79af7ab
MastarCheeze <mon10180218@gmail.com>
2024-09-26 07:47:16 +0800

    add main.ts

---
+++ scripts/layout/main.ts
@@ -0,0 +1,24 @@
+import template from "./template.js";
+import { createBreadcrumb } from "./component/breadcrumb.js";
+import type { BreadcrumbArgs } from "./component/breadcrumb.js";
+import { createDirectory } from "./component/directory.js";
+import type { DirectoryArgs } from "./component/directory.js";
+
+function buildPage(content: string, title: string, breadcrumb: BreadcrumbArgs): string {
+    return template("page", {
+        content: content,
+        title: title,
+        breadcrumb: createBreadcrumb(breadcrumb),
+    });
+}
+
+function buildIndex(content: string, title: string, breadcrumb: BreadcrumbArgs, directory: DirectoryArgs): string {
+    return template("index", {
+        content: content,
+        title: title,
+        breadcrumb: createBreadcrumb(breadcrumb),
+        directory: createDirectory(directory),
+    });
+}
+
+export { buildPage, buildIndex };
