2572943a1da5ee068e4fc8d4fd2a9dd501bf7978
MastarCheeze <mon10180218@gmail.com>
2024-09-26 17:18:59 +0800

    separate export

--- scripts/compile/main.ts
+++ scripts/compile/main.ts
@@ -4,6 +4,8 @@ import directive from "./directive/directive.js";
 
 marked.use({ breaks: true }).use(katex).use(directive);
 
-export function compile(markdown: string) {
+function compile(markdown: string) {
     return marked.parse(markdown) as string;
 }
+
+export { compile };
