4d9d61d81a772f8a38c09d8f53acc20674e00ebd
MastarCheeze <mon10180218@gmail.com>
2024-09-26 17:23:42 +0800

    add parse.ts

---
+++ scripts/io/parse.ts
@@ -0,0 +1,28 @@
+const headerRegex = /^\s*---\s*(?<header>[\s\S]*?)\s*---\s*(?<content>[\s\S]*)/;
+const metadataRegex = /(?<key>\w+)\s*:\s*(?<value>.+)/g;
+
+function parseMetadata(header: string) {
+    const metadata: Record<string, string> = {};
+    while (1) {
+        const match = metadataRegex.exec(header);
+        if (match === null) break;
+
+        const key = match.groups?.key;
+        const value = match.groups?.value;
+        if (key === undefined || value === undefined) throw "Error while parsing metadata";
+
+        metadata[key] = value;
+    }
+    return metadata;
+}
+
+export function parse(markdown: string) {
+    const match = headerRegex.exec(markdown);
+    if (match === null) return { content: markdown, metadata: null };
+
+    const header = match.groups?.header;
+    const content = match.groups?.content;
+    if (header === undefined || content === undefined) throw "Error while parsing markdown";
+
+    return { content: content, metadata: parseMetadata(header) };
+}
