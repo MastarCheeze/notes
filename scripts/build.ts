ce288394db00b50f0ad426c4964258d35d3f6164
MastarCheeze <mon10180218@gmail.com>
2024-09-28 10:40:24 +0800

    add build.ts

---
+++ scripts/build.ts
@@ -0,0 +1,17 @@
+import { SiteBuilder } from "./sitebuilder/main.js";
+import { parseArgv } from "./argv.js";
+
+// parse argv
+const SRC = process.argv[2]; // src
+const OUT = parseArgv("-o", "--output")[1]; // build/public
+const ABS_URL_PREFIX = parseArgv("--abs-url-prefix")[1] ?? "";
+const VERBOSE = parseArgv("-v", "--verbose").length != 0;
+
+if (SRC === undefined) {
+    throw "Please provide a source folder";
+} else if (OUT === undefined) {
+    throw "Please provide an output folder";
+}
+
+const siteBuilder = new SiteBuilder(SRC, OUT);
+siteBuilder.build()
