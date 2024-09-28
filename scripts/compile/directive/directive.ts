285f5a6953ad3f6d6d4f21d42ed290358a679bf7
MastarCheeze <mon10180218@gmail.com>
2024-09-28 15:53:42 +0800

    two column directive

--- scripts/compile/directive/directive.ts
+++ scripts/compile/directive/directive.ts
@@ -1,1 +1,17 @@
-export default {};  // PLACEHOLDER
+import { marked } from "marked";
+import { createDirectives, presetDirectiveConfigs } from "marked-directive";
+
+export default createDirectives([
+    ...presetDirectiveConfigs,
+    {
+        level: "container",
+        marker: "::::",
+        renderer(token) {
+            if (token.meta.name === "columns") {
+                return `<div class="columns">${marked.parse(token.text)}</span>`;
+            }
+
+            return false;
+        },
+    },
+]);
