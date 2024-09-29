02eb0ffa7b215307fb251e4e414fca5d17873afa
MastarCheeze <mon10180218@gmail.com>
2024-09-29 13:34:41 +0800

    implement compiler in sitebuilder

--- scripts/sitebuilder/main.ts
+++ scripts/sitebuilder/main.ts
@@ -4,10 +4,9 @@ import path from "node:path";
 import { parse } from "./parse.js";
 
 import { Entry, Registry } from "./registry.js";
-import { compile } from "../compile/main.js";
+import Compiler from "../compile/main.js";
 import { buildPage, buildIndex } from "../layout/main.js";
 import type { BreadcrumbArgs, DirectoryArgs } from "../layout/main.js";
-import { fixUrls } from "./fix-urls.js";
 
 const INDEX_FILE = "index.md"; // TODO change this to __index.md to make it at the top of the directory
 const TITLE_REGEX = /^\s*#\s+(.*)/; // regex to find the first h1 in a markdown file
@@ -16,6 +15,8 @@ class SiteBuilder {
     private rootSrc: string;
     private rootOut: string;
     private absUrlPrefix: string;
+
+    private compiler: Compiler;
 
     private registry: Registry = new Registry();
     private registerFolderFuncStack: (() => void)[] = [];
@@ -31,16 +32,14 @@ class SiteBuilder {
         this.rootSrc = rootSrc;
         this.rootOut = rootOut;
         this.absUrlPrefix = absUrlPrefix;
+
+        this.compiler = new Compiler();
     }
 
     build() {
         this.log("Compiling...");
         this.buildRecursive(this.rootSrc);
         this.log(`Done compiling: ${this.loggerStats.success} compiled, ${this.loggerStats.error} failed`);
-    }
-
-    private fixUrls(html: string) {
-        return fixUrls(html, this.rootSrc, this.absUrlPrefix);
     }
 
     private buildRecursive(dirSrc: string) {
@@ -86,14 +85,12 @@ class SiteBuilder {
         // check for index file
         let markdown: string = "";
         let metadata: Record<string, string> | null = null;
-        let content = "";
 
         const indexSrc = path.join(src, INDEX_FILE);
         const indexLink = path.join(link, "index.html");
         if (fs.existsSync(indexSrc)) {
             const raw = fs.readFileSync(indexSrc, { encoding: "utf-8" });
             ({ markdown, metadata } = parse(raw));
-            content = compile(markdown);
         }
 
         const title = metadata?.title ?? markdown.match(TITLE_REGEX)?.[1] ?? path.basename(src);
@@ -118,7 +115,7 @@ class SiteBuilder {
         this.registerFolderFuncStack.pop();
         const success = this.registerFolderStatusStack.pop();
         if (success) {
-            this.compileIndex(indexSrc, indexLink, content, entry);
+            this.compileIndex(indexSrc, indexLink, markdown, entry);
         }
         return success;
     }
@@ -130,13 +127,14 @@ class SiteBuilder {
         }
     }
 
-    private compileIndex(src: string, link: string, content: string, entry: Entry) {
+    private compileIndex(src: string, link: string, markdown: string, entry: Entry) {
         const parentSrc = path.dirname(link);
 
-        // build site components and html page
+        // compile page
+        const content = this.compiler.compile(markdown);
         const breadcrumb = this.buildBreadcrumbArgs(parentSrc, false);
         const directory = this.buildDirectoryArgs(entry.subdir!);
-        const html = this.fixUrls(buildIndex(content, entry.title, breadcrumb, directory));
+        const html = buildIndex(content, entry.title, breadcrumb, directory);
 
         // write file
         const out = src.replace(this.rootSrc, this.rootOut).replace(INDEX_FILE, "index.html");
@@ -148,7 +146,6 @@ class SiteBuilder {
         // read and compile markdown
         const raw = fs.readFileSync(src, { encoding: "utf-8" });
         const { markdown, metadata } = parse(raw);
-        const content = compile(markdown);
 
         const title = metadata?.title ?? markdown.match(TITLE_REGEX)?.[1] ?? path.basename(src).replace(".md", ".html");
         const order = metadata?.order ? Number.parseInt(metadata.order) : null;
@@ -162,9 +159,10 @@ class SiteBuilder {
         };
         this.registry.register(link, entry);
 
-        // build site components and html page
+        // compile page
+        const content = this.compiler.compile(markdown);
         const breadcrumb = this.buildBreadcrumbArgs(link, true);
-        const html = this.fixUrls(buildPage(content, entry.title, breadcrumb));
+        const html = buildPage(content, entry.title, breadcrumb);
 
         // write file
         const out = src.replace(this.rootSrc, this.rootOut).replace(".md", ".html");
