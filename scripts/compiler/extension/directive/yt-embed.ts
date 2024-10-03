7a705aeb28429dddd6f90bf2d51e855f475ec8ee
MastarCheeze <mon10180218@gmail.com>
2024-10-03 15:20:10 +0800

    add yt-embed extension

---
+++ scripts/compiler/extension/directive/yt-embed.ts
@@ -0,0 +1,23 @@
+import { DirectiveExtension } from "./types.js";
+
+import type { ExtensionArgs } from "../types.js";
+import type { DirectiveConfig } from "marked-directive";
+
+class YTEmbed extends DirectiveExtension {
+    override directive: DirectiveConfig = {
+        level: "block",
+        marker: "::",
+        renderer(token) {
+            if (token.meta.name === "youtube") {
+                return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${
+                    token.attrs?.vid || ""
+                }" title="${
+                    token.text
+                }" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+            }
+            return false;
+        },
+    };
+}
+
+export default YTEmbed;
